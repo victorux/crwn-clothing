df9bee319d04caefc91a1cacf824e9c99cf22d01
hi.victor <hi.victorb@gmail.com>
2021-05-28 14:08:18 +0100

    created user and cart sagas for sign in, sign out, sign up, clear cart

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -18,16 +19,10 @@ class SignIn extends React.Component{
 
     handleSubmit = async event => {
         event.preventDefault();
-
+        const { emailSignInStart } = this.props;
         const { email, password } = this.state;
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
-        } catch (error) {
-            console.error(error);
-        }
-
+        emailSignInStart(email, password);
     }
 
     handleChange = event => {
@@ -36,6 +31,7 @@ class SignIn extends React.Component{
     }
 
     render() {
+        const { googleSignInStart } = this.props;
         return(
             <div className='sign-in'>
                 <h2 className='title'>I already have an account</h2>
@@ -59,7 +55,7 @@ class SignIn extends React.Component{
                     />
                     <div className="buttons">
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                        <CustomButton type="button" onClick={googleSignInStart} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
@@ -67,4 +63,9 @@ class SignIn extends React.Component{
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+})
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from './root-saga';
+
 import logger from  'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middleWares = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middleWares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
